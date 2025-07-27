b01fecd418c70a7d006787a69495271ce0850b65
SRINIVAS-C-M <110298859+SRINIVAS-C-M@users.noreply.github.com>
2025-07-27 21:39:26 +0530

    Create config.py

---
+++ GUI for Weather Monitoring and Analysis/config.py
@@ -0,0 +1,9 @@
+API_KEY = "your_openweathermap_api_key"
+CITY = "Chennai"
+POSTGRES = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "weatherdb",
+    "user": "postgres",
+    "password": "your_password"
+}
