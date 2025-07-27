94f5a6c3cf30940d309f883bc01890eaa260fcd1
SRINIVAS-C-M <110298859+SRINIVAS-C-M@users.noreply.github.com>
2025-07-27 21:40:03 +0530

    Update db.py

--- GUI for Weather Monitoring and Analysis/db.py
+++ GUI for Weather Monitoring and Analysis/db.py
@@ -1,1 +1,29 @@
+import psycopg2
+from config import POSTGRES
 
+def connect_db():
+    return psycopg2.connect(**POSTGRES)
+
+def create_tables():
+    with connect_db() as conn:
+        cur = conn.cursor()
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS weather_raw (
+            id SERIAL PRIMARY KEY,
+            city TEXT,
+            timestamp TIMESTAMP,
+            data JSONB
+        );
+        """)
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS weather_cleaned (
+            id SERIAL PRIMARY KEY,
+            city TEXT,
+            timestamp TIMESTAMP,
+            temp_c FLOAT,
+            humidity INT,
+            wind_speed FLOAT,
+            temp_deviation FLOAT
+        );
+        """)
+        conn.commit()
